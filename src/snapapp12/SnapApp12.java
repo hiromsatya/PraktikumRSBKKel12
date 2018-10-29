fbf03affb7da805eaead94225939c71efd3d6b2b
hiromsatya <hhsatyanegara@student.ce.undip.ac.id>
2018-10-29 11:26:54 +0700

    Upload Pertama

---
+++ src/snapapp12/SnapApp12.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp12;
+
+/**
+ *
+ * @author HIROMI
+ */
+public class SnapApp12 extends SnapFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapApp12 a = new SnapApp12();
+        a.setVisible(true);
+    }
+    
+}
